3a1ca4b312621c01e0b1d7feb33de3bdaf2bd0d0
m19e <nexus.me7290@gmail.com>
2019-07-14 23:08:07 +0900

    メッセージGET/POSTが動きました cssはおいおい

--- pages/messages/index.vue
+++ pages/messages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container m-5">
-    <div v-for="(item, i) in messages" :key="i">
-      {{ item }}
-    </div>
+  <div class="container-fluid m-5">
+    <ul v-for="(msg, i) in messages" style="list-style:none;" class="m-4" :key="i">
+      <li v-show="msg.timestamp">{{ msg }}</li>
+    </ul>
 
-    <b-form inline>
-      <b-input v-model="message" class="mb-2 mr-sm-2 mb-sm-0" placeholder="メッセージを入力" />
+    <b-form class="fixed-bottom m-2" inline>
+      <b-input v-model="message" class="mb-2 mr-sm-2 mb-sm-0 col-10" placeholder="メッセージを入力" />
       <b-button @click="addMessage" variant="primary">送信</b-button>
     </b-form>
   </div>
